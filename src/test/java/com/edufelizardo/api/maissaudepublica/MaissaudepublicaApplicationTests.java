5be1923b145a3e76302b20505005cdaed1b035e6
Eduardo Felizardo <edufelizardo1@gmail.com>
2023-05-06 14:24:14 -0300

    Initial project

---
+++ src/test/java/com/edufelizardo/api/maissaudepublica/MaissaudepublicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufelizardo.api.maissaudepublica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaissaudepublicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
